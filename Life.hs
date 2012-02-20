16b285e80eb459e7a151fca96b4ccc68406bd9ae
Nicholas Bergson-Shilcock <me@nicholasbs.net>
2012-02-20 14:05:24 -0500

    general code cleanup

--- Life.hs
+++ Life.hs
@@ -1,6 +1,8 @@
 module Life where
 
 import Data.List (nub, union)
+import System.Posix (sleep, nanosleep)
+import System.Console.ANSI (clearScreen)
 
 type Cell = (Int, Int) -- a cell on the board
 type Board = [Cell]    -- a board is a list of living cells
@@ -9,30 +11,18 @@ type Board = [Cell]    -- a board is a list of living cells
 -- _XXXXX_
 -- __XX___
 -- _______
--- (the board extends infinitely down and to the left);
---
-board :: Board
-board = [(1,0),(2,0),(3,0),(4,0),(5,0),(2,1),(3,1)]
-
-squareBoard :: Board
-squareBoard = [(2,2),(2,3),(3,2),(3,3)]
-
-glider :: Board
-glider = [(3,0),(3,1),(3,2),(2,2),(1,1)]
-
-bigBoard = [(1,0),(2,0),(3,0),(4,0),(5,0),(2,1),(3,1),(10,2),(11,2),(11,0),(-2,-4),(-2,-5),(-2,-6)]
+-- (the board extends infinitely);
 
 neighbors :: Cell -> [Cell]
-neighbors cell = 
-  let deltas = [(-1,-1),(0,-1),(1,-1),(1,0),(1,1),(0,1),(-1,1),(-1,0)] 
+neighbors cell =
+  let deltas = [(-1,-1),(0,-1),(1,-1),(1,0),(1,1),(0,1),(-1,1),(-1,0)]
   in map (\(x, y) -> (fst cell + x, snd cell + y)) deltas
 
 alive :: Board -> Cell -> Bool
 alive board cell = cell `elem` board
 
 livingNeighbors :: Board -> Cell -> [Cell]
-livingNeighbors board cell =
-  filter (alive board) (neighbors cell)
+livingNeighbors board cell = filter (alive board) (neighbors cell)
 
 numLivingNeighbors :: Board -> Cell -> Int
 numLivingNeighbors board cell = length (livingNeighbors board cell)
@@ -46,33 +36,55 @@ deadCellsWithLivingNeighbor living =
   let all = cellsWithLivingNeighbor living
   in filter (\c -> not $ c `elem` living) all
 
+-- Rule: Dead cells with exactly 3 living neighbors become live cells
 birthCells :: Board -> Board
 birthCells board =
   let potentialKids = deadCellsWithLivingNeighbor board
   in foldl (\acc c -> if numLivingNeighbors board c == 3 then c : acc else acc) [] potentialKids
 
+-- Rule: Living cells with fewer than 2 living neighbors die
 removeStarvedCells board = filter (\c -> numLivingNeighbors board c >= 2) board
 
+-- Rule: Living cells with more than 3 living neighbors die
 removeOverpopulatedCells board = filter (\c -> numLivingNeighbors board c <= 3) board
 
 next :: Board -> Board
 next board = union (removeOverpopulatedCells $ removeStarvedCells board) (birthCells board)
-  
 
--- play and display functions taken from
+
+-- Test boards
+board :: Board
+board = [(1,0),(2,0),(3,0),(4,0),(5,0),(2,1),(3,1),(10,2),(11,2),(11,0),(-2,-4),(-2,-5),(-2,-6)]
+
+glider :: Board
+glider = [(3,0),(3,1),(3,2),(2,2),(1,1)]
+
+
+-- play and display function inspired by
 -- https://github.com/nbartlomiej/gameofhaskell/blob/master/GameOfHaskell.hs
-main = play bigBoard
+
+terminalCols = 79
+terminalRows = 24
+
+terminalLeft = 0 - terminalCols `div` 2
+terminalRight = terminalCols `div` 2
+terminalTop = 0 - terminalRows `div` 2
+terminalBottom = terminalRows `div` 2
+
+main = do
+  clearScreen
+  play board
 
 play :: Board -> IO ()
 play board = do
   display board
-  getLine
+  getLine -- comment out and uncomment below for "animation"
+  -- nanosleep 200000000 -- 200 ms
   play $ next board
 
 display :: Board -> IO ()
 display board = do
   mapM_ (\x -> do
-    mapM_ (\y -> putChar $ if (y,x) `elem` board then 'x' else ' ')[-10 .. 20]
+    mapM_ (\y -> putChar $ if alive board (y,x) then 'X' else ' ') [terminalLeft .. terminalRight]
     putChar '\n'
-    )[-10 .. 20]
-
+    ) [terminalTop .. terminalBottom]
